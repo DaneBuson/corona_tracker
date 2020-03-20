e85363d5ce9e640cfd55e7e5d821612a2e5e678f
Dane Buson <dane.buson@gmail.com>
2020-03-20 14:44:12 -0700

    Added weather display

--- README.md
+++ README.md
@@ -9,6 +9,12 @@ The display portion is mostly cribbed from their stats.py program in the example
 
 It pulls from two urls, one for the world count, one for the US.
 
+Additionally I've added the ability to display weather, and moved the URLs and city values into a config file.
+
+In order to use weather you have to go to openweathermap.org and get an api_key and put in the config file.
+
+It will switch between the two kinds of display for whatever duration is in the config (10 seconds by default)
+
 ```
 https://www.worldometers.info/coronavirus/
 https://www.worldometers.info/coronavirus/country/us/
@@ -22,5 +28,5 @@ Install pip3 and the other python3 libararies needed
 
 ```
 sudo apt install python3-pip libopenjp2-7-dev libopenjp2-7    
-sudo python3 -m pip install beautifulsoup4 requests Adafruit-SSD1306 pillow RPi.GPIO
+sudo python3 -m pip install beautifulsoup4 requests Adafruit-SSD1306 pillow RPi.GPIO pyown
 ```
--- corona_stats.py
+++ corona_stats.py
@@ -33,6 +33,7 @@ import configparser
 import re
 import requests
 import time
+import pyowm
 from bs4 import BeautifulSoup
 
 
@@ -48,6 +49,13 @@ if (corona_refresh < 1140):
 wo_url = config['Corona']['world']
 reg_url = config['Corona']['region']
 reg_name = config['Corona']['region_name']
+
+weather_refresh = int(config['Weather']['refresh'])
+if (weather_refresh < 300):
+    weather_refresh = 300
+
+#how long each 'frame' stays up
+duration = int(config['General']['duration'])
 
 # Raspberry Pi pin configuration:
 RST = None     # on the PiOLED this pin isnt used
@@ -95,13 +103,18 @@ font = ImageFont.load_default()
 # font = ImageFont.truetype('Minecraftia.ttf', 8)
 
 def get_corona(url):
-    response = requests.get(url)
+    try:
+        response = requests.get(url)
+    except ConnectionError:
+        corona = {'Error': "Connection"}
+        return 
+        
     soup = BeautifulSoup(response.text, "html.parser")
 
     #with open("index.html") as fp:
     #    soup = BeautifulSoup(fp, "html.parser")
 
-    corona = {'Deaths': 0, 'Infect': 0, 'Recove': 0}
+    corona = {'Deaths': 0, 'Infect': 0, 'Recove': 0, 'Error': "None"}
 
     nomatch = 0
     line_count = 1 
@@ -131,6 +144,53 @@ def get_corona(url):
                 #print("    Statnum = ", statnum)
     return corona 
 
+def get_weather_data(config):
+    cities = {"city_num":0}
+
+    c_weather = config['Weather']
+
+    api_key = config['Weather']['ow_apikey']
+
+    #connect to openweather with our key
+    owm = pyowm.OWM(api_key)
+
+    city_list = []
+    for jj in ('city1','city2','city3'):
+        if (jj in c_weather.keys()):
+            tcity = c_weather[jj].strip()
+            city_list.append(c_weather[jj])
+
+    #TODO - units SI/Imperial
+    city_num = 0
+    for city in city_list:
+        print ("Getting own for '",city,"'\n")
+        observation = owm.weather_at_place(city)
+        w = observation.get_weather()
+
+        CC = "CC"+str(city_num)
+        cities[CC] = {'name': city}
+        cities[CC]['wind_speed'] = w.get_wind()['speed']
+        #cities[CC]['wind_deg'] = w.get_wind()['deg'] #direction
+
+        #tempF = w.get_temperature('fahrenheit')
+        temp =  w.get_temperature('celsius')
+
+        cities[CC]['temp'] = {'temp_cur': int(temp['temp']), 'temp_max': int(temp['temp_max']), 'temp_min': int(temp['temp_min'])}
+        cities[CC]['humidity'] = w.get_humidity()
+        status = w.get_status()
+        cities[CC]['status'] = status
+
+        #print ("\tWeather in ",city," is ",status)
+        city_num += 1
+
+    cmd = "date |cut -c 11-23"
+    stdoutdata = subprocess.getoutput(cmd)
+    cities['timestamp'] = "Time: " + stdoutdata.split()[0] + " PDT"
+    
+    cities['city_num'] = city_num
+    return cities
+
+
 def get_max_string (*args):
     max = 0
 
@@ -140,53 +200,101 @@ def get_max_string (*args):
 
     return max
 
+
+last_refresh = {'corona':0,'weather':0}
+wo_infected = ""
+wo_deaths = ""
+reg_infected = ""
+reg_deaths = ""
+
+display_frame = 0
+display_city = 0
+cities = {}
+
 # start main loop
 while True:
 
+    if ((time.time() - last_refresh['corona'])  > corona_refresh):
+        #get world corona
+        errors = 0
+        wo_corona = get_corona(wo_url)
+        if (wo_corona['Error'] != 'None'):
+            print ("Error while trying to get world stats\n\t",wo_corona['Error'])
+            errors += 1
+
+        #get regional corona (us)
+        reg_corona = get_corona(reg_url)
+        if (reg_corona['Error'] != 'None'):
+            print ("Error while trying to get regional stats\n\t",reg_corona['Error'])
+            errors += 1
+
+        if (errors == 0):
+            padding = get_max_string( wo_corona['Infect'], wo_corona['Deaths'], reg_corona['Infect'], reg_corona['Deaths'])
+
+            # format world #'s
+            wo_infected = "Infected: " + wo_corona['Infect'].rjust(padding)
+            wo_deaths   = "Deaths  : " + wo_corona['Deaths'].rjust(padding)
+
+            # format regional #'s
+            reg_infected = "Infected: " + reg_corona['Infect'].rjust(padding)
+            reg_deaths   = "Deaths  : " + reg_corona['Deaths'].rjust(padding)
+
+            cmd = "date |cut -c 11-23"
+            stdoutdata = subprocess.getoutput(cmd)
+            curTime = "CVD-19 @ " + stdoutdata.split()[0] + " PDT"
+            cmd = " date |cut -c 5-11,25-30"
+            stdoutdata = subprocess.getoutput(cmd)
+            #curDate = " -- " +  stdoutdata.split()[0-2] + " -- "
+            curDate = "  -- " +  stdoutdata + " -- "
+    
+            print (curTime," - ",curDate,"\n")
+            last_refresh['corona'] = time.time()
+        else:
+            print ("Skipping refresh - errors trying to get data")
+
+    if ((time.time() - last_refresh['weather'])  > weather_refresh):
+        cities = get_weather_data (config)
+        last_refresh['weather'] = time.time()
+
+    # Drawscreen
     # Draw a black filled box to clear the image.
     draw.rectangle((0,0,width,height), outline=0, fill=0)
+    
+    if (display_frame == 0): #draw corona
+        #print ("\nDisplaying CVD-19 stats")
+        draw.text((x, top),        str(curTime),  font=font, fill=255)
+        draw.text((x, top+8),        str(curDate),  font=font, fill=255)
+        draw.text((x, top+16),    str("World #"),  font=font, fill=255)
+        draw.text((x, top+24),     str(wo_infected), font=font, fill=255)
+        draw.text((x, top+32),    str(wo_deaths),  font=font, fill=255)
 
-    #get world corona
-    wo_corona = get_corona(wo_url)
+        draw.text((x, top+41),    str(reg_name+" #"),  font=font, fill=255)
+        draw.text((x, top+49),     str(reg_infected), font=font, fill=255)
+        draw.text((x, top+57),    str(reg_deaths),  font=font, fill=255)
+    elif (display_frame == 1): #draw one of the cities
+        cmd = " date |cut -c 5-11,25-30"
+        stdoutdata = subprocess.getoutput(cmd)
+        curDate = "  -- " +  stdoutdata + " -- "
 
-    #get regional corona (us)
-    reg_corona = get_corona(reg_url)
+        city = cities["CC"+str(display_city)]
+        #print ("\nDisplaying Weather for ",city['name'], "- DC = ",display_city)
+        draw.text((x, top),      str(cities['timestamp']), font=font, fill=255)
+        draw.text((x, top+8),      str(curDate), font=font, fill=255)
+        draw.text((x, top+16),    str("City: "+city['name']),  font=font, fill=255)
 
-    padding = get_max_string( wo_corona['Infect'], wo_corona['Deaths'], reg_corona['Infect'], reg_corona['Deaths'])
+        temp_str1 = "Temp: " + str(city['temp']['temp_cur'])
+        temp_str2 = "Max/Min: " + str(city['temp']['temp_max']) + "/" + str(city['temp']['temp_min'])
+        draw.text((x, top+24),    str(temp_str1),  font=font, fill=255)
+        draw.text((x, top+32),    str(temp_str2),  font=font, fill=255)
+        draw.text((x, top+48),    str("Cond: "+str(city['status'])),  font=font, fill=255)
+        draw.text((x, top+40),    str("Wind: "+str(city['wind_speed'])),  font=font, fill=255)
 
-    # format world #'s
-    wo_infected = "Infected: " + wo_corona['Infect'].rjust(padding)
-    wo_deaths   = "Deaths  : " + wo_corona['Deaths'].rjust(padding)
+        display_city = (display_city + 1) % cities['city_num']
 
-    # format regional #'s
-    reg_infected = "Infected: " + reg_corona['Infect'].rjust(padding)
-    reg_deaths   = "Deaths  : " + reg_corona['Deaths'].rjust(padding)
-
-    cmd = "date |cut -c 11-23"
-    stdoutdata = subprocess.getoutput(cmd)
-    curTime = "CVD-19 @ " + stdoutdata.split()[0] + " PDT"
-    print (curTime,"\n")
-
-    cmd = " date |cut -c 5-11,25-30"
-    stdoutdata = subprocess.getoutput(cmd)
-    #curDate = " -- " +  stdoutdata.split()[0-2] + " -- "
-    curDate = "  -- " +  stdoutdata + " -- "
-    print (curDate,"\n")
-
-    draw.text((x, top),        str(curTime),  font=font, fill=255)
-    draw.text((x, top+8),        str(curDate),  font=font, fill=255)
-    draw.text((x, top+16),    str("World #"),  font=font, fill=255)
-    draw.text((x, top+24),     str(wo_infected), font=font, fill=255)
-    draw.text((x, top+32),    str(wo_deaths),  font=font, fill=255)
-
-    draw.text((x, top+41),    str(reg_name+" #"),  font=font, fill=255)
-    draw.text((x, top+49),     str(reg_infected), font=font, fill=255)
-    draw.text((x, top+57),    str(reg_deaths),  font=font, fill=255)
-
-    #draw.text((x, top+25),    str(Disk),  font=font, fill=255)
+    display_frame = (display_frame + 1) %2
 
     # Display image.
     disp.image(image)
     disp.display()
-    time.sleep(corona_refresh) 
+    time.sleep(duration) 
 
